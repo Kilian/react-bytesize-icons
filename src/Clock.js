7571b3d7a3bb4b57b5a96214b23b13440c725e75
Kilian Valkhof <kilian@kilianvalkhof.com>
2020-06-15 12:35:18 +0200

    fix typo

--- src/Clock.js
+++ src/Clock.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Clock = ({d="i-clock", className, width=32, height=32, strokeWidth='6.25%', strokeLinejoin='round', strokeLinecap='round', color='currentcolor'}) => (
+const Clock = ({id="i-clock", className, width=32, height=32, strokeWidth='6.25%', strokeLinejoin='round', strokeLinecap='round', color='currentcolor'}) => (
   <svg className={className} id={id} iviewBox="0 0 32 32" width={width} height={height} fill="none" stroke={color} strokeLinecap={strokeLinecap} strokeLinejoin={strokeLinejoin} strokeWidth={strokeWidth}>
     <circle cx="16" cy="16" r="14" />
     <path d="M16 8 L16 16 20 20" />
